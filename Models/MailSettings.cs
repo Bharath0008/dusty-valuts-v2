2ee88fee5ad36a7adab54ee52b9d97d7db356c39
Bharath <bharathkumar82000@gmail.com>
2025-07-12 17:20:09 +0530

    Warning issue fixed mailsetting

--- Models/MailSettings.cs
+++ Models/MailSettings.cs
@@ -2,11 +2,11 @@
 {
     public class MailSettings
     {
-        public string Host { get; set; }
+        public string? Host { get; set; }
         public int Port { get; set; }
-        public string UserName { get; set; }
-        public string Password { get; set; }
-        public string FromEmail { get; set; }
+        public string? UserName { get; set; }
+        public string? Password { get; set; }
+        public string? FromEmail { get; set; }
         public bool EnableSsl { get; set; }
     }
 }
