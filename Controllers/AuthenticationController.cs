97ecbc2e4214ecdb80d17d027b3da18d82c2d74a
Bharath <bharathkumar82000@gmail.com>
2025-07-12 17:53:55 +0530

    Jenkine Test

--- Controllers/AuthenticationController.cs
+++ Controllers/AuthenticationController.cs
@@ -90,11 +90,11 @@ namespace LinkedInAPI.Controllers
 }
 public class Root
 {
-    public string sub { get; set; }
+    public string? sub { get; set; }
     public bool email_verified { get; set; }
-    public string name { get; set; }
-    public string given_name { get; set; }
-    public string family_name { get; set; }
-    public string email { get; set; }
-    public string picture { get; set; }
+    public string? name { get; set; }
+    public string? given_name { get; set; }
+    public string? family_name { get; set; }
+    public string? email { get; set; }
+    public string? picture { get; set; }
 }
